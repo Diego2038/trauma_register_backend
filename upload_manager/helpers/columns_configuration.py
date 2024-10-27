4f53f591c261e49632742d6da51f1631dab834c4
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2024-10-27 17:48:23 -0500

    V0.4.0 New: Now it's possible to save Patient Data information in Postgres DB

--- upload_manager/helpers/columns_configuration.py
+++ upload_manager/helpers/columns_configuration.py
@@ -1,5 +1,18 @@
-
+import enum
+from medical_records.models import *
+from django.db.models import Model
 #! NOTE: If you want to create two or more tables from one sheet, you must finish with the character ":" followed by its numeral 
+
+class DataTypeCell(enum.Enum):
+  STRING = "string"
+  INT = "int"
+  TIMESTAMP = "timestamp"
+  DATE = "date"
+  TIME = "time"
+  DECIMAL = "decimal"
+  BOOLEAN = "boolean"
+  TEXT = "text"
+
 columns_ubication : dict[str, dict[str,str]] = {  
     "Trauma Register:1": { #* Patient Data
       "trauma_register_record_id": "A", 
@@ -270,5 +283,349 @@ columns_ubication : dict[str, dict[str,str]] = {
       "gcs_verbal" : "R",
       "gcs_total" : "S",
       "avup" : "T",
+    },
+}
+
+column_name_type_to_model : dict[str,dict[str,Model|dict[str,DataTypeCell]]] = {  
+    "Trauma Register:1": {
+      "model": PatientData,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "direccion_linea_1": DataTypeCell.STRING,
+        "direccion_linea_2": DataTypeCell.STRING,
+        "ciudad": DataTypeCell.STRING,
+        "canton_municipio": DataTypeCell.STRING,
+        "provincia_estado": DataTypeCell.STRING,
+        "codigo_postal": DataTypeCell.STRING,
+        "pais": DataTypeCell.STRING,
+        "edad": DataTypeCell.INT,
+        "unidad_de_edad": DataTypeCell.STRING,
+        "genero": DataTypeCell.STRING,
+        "fecha_de_nacimiento": DataTypeCell.DATE,
+        "ocupacion": DataTypeCell.STRING,
+        "estado_civil": DataTypeCell.STRING,
+        "nacionalidad": DataTypeCell.STRING,
+        "grupo_etnico": DataTypeCell.STRING,
+        "otro_grupo_etnico": DataTypeCell.STRING,
+        "num_doc_de_identificacion": DataTypeCell.STRING,
+      }
+    },
+    "Trauma Register:2": {
+      "model": HealthcareRecord,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "numero_de_historia_clinica": DataTypeCell.STRING,
+        "hospital": DataTypeCell.STRING,
+        "fecha_y_hora_de_llegada_del_paciente": DataTypeCell.TIMESTAMP,
+        "referido": DataTypeCell.BOOLEAN,
+        "policia_notificada": DataTypeCell.BOOLEAN,
+        "fecha_y_hora_de_llegada_del_medico": DataTypeCell.TIMESTAMP,
+        "fecha_y_hora_de_notificacion_al_medico": DataTypeCell.TIMESTAMP,
+        "alerta_equipo_de_trauma": DataTypeCell.BOOLEAN,
+        "nivel_de_alerta": DataTypeCell.STRING,
+        "paciente_asegurado": DataTypeCell.BOOLEAN,
+        "tipo_de_seguro": DataTypeCell.STRING,
+        "motivo_de_consulta": DataTypeCell.TEXT,
+        "inmunizacion_contra_el_tetanos": DataTypeCell.STRING,
+        "descripcion_del_examen_fisico": DataTypeCell.TEXT,
+        "mecanismo_primario": DataTypeCell.STRING,
+        "numero_de_lesiones_serias": DataTypeCell.STRING,
+        "descripcion_del_diagnostico": DataTypeCell.TEXT,
+        "disposicion_o_destino_del_paciente": DataTypeCell.STRING,
+        "donacion_de_organos": DataTypeCell.STRING,
+        "autopsia": DataTypeCell.STRING,
+        "muerte_prevenible": DataTypeCell.STRING,
+        "tipo_de_admision": DataTypeCell.STRING,
+        "fecha_y_hora_de_la_disposicion": DataTypeCell.TIMESTAMP,
+        "tiempo_en_sala_de_emergencias_hora": DataTypeCell.INT,
+        "tiempo_en_sala_de_emergencias_minutos": DataTypeCell.INT,
+        "numero_de_referencia_del_ed": DataTypeCell.STRING,
+        "fecha_de_admision": DataTypeCell.DATE,
+        "fecha_y_hora_de_alta": DataTypeCell.TIMESTAMP,
+        "dias_de_hospitalizacion": DataTypeCell.DECIMAL,
+        "uci_dias": DataTypeCell.INT,
+        "detalles_de_hospitalizacion": DataTypeCell.TEXT,
+        "disposicion_o_destino_del_paciente_del_hospitalizacion": DataTypeCell.STRING,
+        "donacion_de_organos_del_hospitalizacion": DataTypeCell.STRING,
+        "autopsia_del_hospitalizacion": DataTypeCell.STRING,
+        "muerte_prevenible_del_hospitalizacion": DataTypeCell.STRING,
+        "numero_de_referencia_del_hospitalizacion": DataTypeCell.STRING,
+        "agencia_de_transporte": DataTypeCell.STRING,
+        "origen_del_transporte": DataTypeCell.STRING,
+        "numero_de_registro_del_transporte": DataTypeCell.STRING,
+        "fecha_y_hora_de_notificacion_pre_hospitalaria": DataTypeCell.TIMESTAMP,
+        "fecha_y_hora_de_llegada_a_la_escena": DataTypeCell.TIMESTAMP,
+        "fecha_y_hora_de_salida_de_la_escena": DataTypeCell.TIMESTAMP,
+        "razon_de_la_demora": DataTypeCell.STRING,
+        "reporte_o_formulario_pre_hospitalario_entregado": DataTypeCell.BOOLEAN,
+        "ciudad_hospital_mas_cercano_al_sitio_del_incidente": DataTypeCell.STRING,
+        "tiempo_de_extricacion_hora": DataTypeCell.INT,
+        "tiempo_de_extricacion_minutos": DataTypeCell.INT,
+        "duracion_del_transporte_horas": DataTypeCell.INT,
+        "duracion_del_transporte_minutos": DataTypeCell.INT,
+        "procedimiento_realizado": DataTypeCell.TEXT,
+        "frecuencia_cardiaca_en_la_escena": DataTypeCell.INT,
+        "presion_arterial_sistolica_en_la_escena": DataTypeCell.INT,
+        "presion_arterial_diastolica_en_la_escena": DataTypeCell.INT,
+        "frecuencia_respiratoria_en_la_escena": DataTypeCell.INT,
+        "calificador_de_frecuencia_respiratoria_en_la_escena": DataTypeCell.STRING,
+        "temperatura_en_la_escena_celsius": DataTypeCell.DECIMAL,
+        "saturacion_de_o2_en_la_escena": DataTypeCell.INT,
+        "frecuencia_cardiaca_durante_el_transporte": DataTypeCell.INT,
+        "presion_arterial_sistolica_de_transporte": DataTypeCell.INT,
+        "presion_diastolica_durante_el_transporte": DataTypeCell.INT,
+        "frecuencia_respiratoria_durante_el_transporte": DataTypeCell.INT,
+        "calificador_de_frecuencia_respiratoria_durante_el_transporte": DataTypeCell.STRING,
+        "temperatura_durante_el_transporte_celsius": DataTypeCell.DECIMAL,
+        "saturacion_de_o2_durante_el_transporte": DataTypeCell.INT,
+        "perdida_de_conciencia": DataTypeCell.INT,
+        "duracion_de_perdida_de_conciencia": DataTypeCell.TIME,
+        "gcs_ocular": DataTypeCell.INT,
+        "gcs_verbal": DataTypeCell.INT,
+        "gcs_motora": DataTypeCell.INT,
+        "gcs_total": DataTypeCell.INT,
+        "sangre_l": DataTypeCell.DECIMAL,
+        "coloides_l": DataTypeCell.DECIMAL,
+        "cristaloides_l": DataTypeCell.DECIMAL,
+        "hallazgos_clinicos_texto": DataTypeCell.TEXT,
+        "fecha_y_hora_de_envio_de_contra_referencia": DataTypeCell.TIMESTAMP,
+        "fecha_de_alta_de_contrar_referencia": DataTypeCell.TIMESTAMP,
+        "hallazgos_clinicos_existencia": DataTypeCell.BOOLEAN,
+        "servicio_que_atendio": DataTypeCell.STRING,
+        "paciente_admitido": DataTypeCell.BOOLEAN,
+        "hospital_que_recibe": DataTypeCell.STRING,
+        "otro_servicio": DataTypeCell.STRING,
+        "servicio_que_recibe": DataTypeCell.STRING,
+        "recomendaciones": DataTypeCell.TEXT,
+        "numero_de_referencia_de_referencias_salientes": DataTypeCell.STRING,
+        "fecha_de_envio_de_referencia": DataTypeCell.TIMESTAMP,
+        "fecha_de_referencia": DataTypeCell.DATE,
+        "razon_de_la_referencia": DataTypeCell.TEXT,
+        "medico_que_refiere": DataTypeCell.STRING,
+        "estado_de_referencia": DataTypeCell.STRING,
+        "fecha_de_aceptacion_de_referencia": DataTypeCell.DATE,
+        "iss": DataTypeCell.INT,
+        "kts": DataTypeCell.INT,
+        "rts": DataTypeCell.DECIMAL,
+        "abdomen": DataTypeCell.INT,
+        "torax": DataTypeCell.INT,
+        "externo": DataTypeCell.INT,
+        "extremidades": DataTypeCell.INT,
+        "cara": DataTypeCell.INT,
+        "cabeza": DataTypeCell.INT,
+        "triss_contuso": DataTypeCell.DECIMAL,
+        "triss_penetrante": DataTypeCell.DECIMAL,
+      }
+    },
+    "Trauma Register:3": {
+      "model": InjuryRecord,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "consumo_de_alcohol": DataTypeCell.STRING,
+        "valor_de_alcoholemia": DataTypeCell.DECIMAL,
+        "unidad_de_alcohol": DataTypeCell.STRING,
+        "otra_sustancia_de_abuso": DataTypeCell.STRING,
+        "direccion_nombre_del_lugar": DataTypeCell.STRING,
+        "ciudad_de_evento_de_la_lesion": DataTypeCell.STRING,
+        "condado_de_lesiones": DataTypeCell.STRING,
+        "estado_provincia_de_lesiones": DataTypeCell.STRING,
+        "pais_de_lesiones": DataTypeCell.STRING,
+        "codigo_postal_de_lesiones": DataTypeCell.STRING,
+        "fecha_y_hora_del_evento": DataTypeCell.TIMESTAMP,
+        "accidente_de_trafico": DataTypeCell.BOOLEAN,
+        "tipo_de_vehiculo": DataTypeCell.STRING,
+        "ocupante": DataTypeCell.STRING,
+        "velocidad_de_colision": DataTypeCell.STRING,
+        "scq": DataTypeCell.INT,
+        "caida": DataTypeCell.BOOLEAN,
+        "altura_metros": DataTypeCell.DECIMAL,
+        "tipo_de_superficie": DataTypeCell.STRING,
+      }
+    },
+    "Collision": {
+      "model": Collision,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_colision": DataTypeCell.STRING,
+      }
+    },
+    "Drug Abuse": {
+      "model": DrugAbuse,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_droga": DataTypeCell.STRING,
+      }
+    },
+    "Vital Sign GCS Qualifier": {
+      "model": VitalSignGcsQualifier,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "calificador_gcs": DataTypeCell.STRING,
+      }
+    },
+    "Hospitalization Variable": {
+      "model": HospitalizationVariable,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_variable": DataTypeCell.STRING,
+        "valor_de_la_variable": DataTypeCell.STRING,
+        "fecha_y_hora_de_la_variable": DataTypeCell.TIMESTAMP,
+        "localizacion_de_variable": DataTypeCell.STRING,
+      }
+    },
+    "Hospitalization Complication": {
+      "model": HospitalizationComplication,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_complicacion": DataTypeCell.STRING,
+        "fecha_y_hora_de_complicacion": DataTypeCell.TIMESTAMP,
+        "lugar_de_complicacion": DataTypeCell.STRING,
+      }
+    },
+    "Trauma Register ICD10": {
+      "model": TraumaRegisterIcd10,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "descripcion": DataTypeCell.TEXT,
+        "mecanismo_icd": DataTypeCell.STRING,
+      }
+    },
+    "Intensive Care Unit": {
+      "model": IntensiveCareUnit,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo": DataTypeCell.STRING,
+        "fecha_y_hora_de_inicio": DataTypeCell.TIMESTAMP,
+        "fecha_y_hora_de_termino": DataTypeCell.TIMESTAMP,
+        "lugar": DataTypeCell.STRING,
+        "icu_days": DataTypeCell.DECIMAL,
+      }
+    },
+    "Imaging": {
+      "model": Imaging,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_imagen": DataTypeCell.STRING,
+        "parte_del_cuerpo": DataTypeCell.STRING,
+        "opcion": DataTypeCell.BOOLEAN,
+        "descripcion": DataTypeCell.TEXT,
+      }
+    },
+    "Apparent Intent Injury": {
+      "model": ApparentIntentInjury,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "intencion_aparente": DataTypeCell.STRING,
+      }
+    },
+    "Burn Injury": {
+      "model": BurnInjury,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_quemadura": DataTypeCell.STRING,
+        "grado_de_quemadura": DataTypeCell.STRING,
+      }
+    },
+    "Firearm Injury": {
+      "model": FirearmInjury,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_arma_de_fuego": DataTypeCell.STRING,
+      }
+    },
+    "Penetrating Injury": {
+      "model": PenetratingInjury,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_lesion_penetrante": DataTypeCell.STRING,
+      }
+    },
+    "Poisoning Injury": {
+      "model": PoisoningInjury,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_envenenamiento": DataTypeCell.STRING,
+      }
+    },
+    "Violence Injury": {
+      "model": ViolenceInjury,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_violencia": DataTypeCell.STRING,
+      }
+    },
+    "Device": {
+      "model": Device,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "tipo_de_dispositivo": DataTypeCell.STRING,
+      }
+    },
+    "Laboratory": {
+      "model": Laboratory,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "resultado_de_laboratorio": DataTypeCell.STRING,
+        "fecha_y_hora_de_laboratorio": DataTypeCell.TIMESTAMP,
+        "nombre_del_laboratorio": DataTypeCell.STRING,
+        "nombre_de_la_unidad_de_laboratorio": DataTypeCell.STRING,
+      }
+    },
+    "Physical Exam Body Part Injury": {
+      "model": PhysicalExamBodyPartInjury,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "parte_del_cuerpo": DataTypeCell.STRING,
+        "tipo_de_lesion": DataTypeCell.STRING,
+      }
+    },
+    "Procedure": {
+      "model": Procedure,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "procedimiento_realizado": DataTypeCell.STRING,
+        "fecha_y_hora_de_inicio": DataTypeCell.TIMESTAMP,
+        "fecha_y_hora_de_termino": DataTypeCell.TIMESTAMP,
+        "lugar": DataTypeCell.STRING,
+      }
+    },
+    "Prehospital Procedure": {
+      "model": PrehospitalProcedure,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "procedimiento_realizado": DataTypeCell.STRING,
+      }
+    },
+    "Transportation Mode": {
+      "model": TransportationMode,
+      "type": {
+        "trauma_register_record_id": DataTypeCell.INT,
+        "modo_de_transporte": DataTypeCell.STRING,
+      }
+    },
+    "Vital Sign": {
+      "model": VitalSign,
+      "type": {
+        "record_id": DataTypeCell.INT,
+        "trauma_register_record_id": DataTypeCell.INT,
+        "fecha_y_hora_de_signos_vitales": DataTypeCell.TIMESTAMP,
+        "signos_de_vida": DataTypeCell.BOOLEAN,
+        "frecuencia_cardiaca": DataTypeCell.INT,
+        "presion_arterial_sistolica": DataTypeCell.INT,
+        "presion_arterial_diastolica": DataTypeCell.INT,
+        "frecuencia_respiratoria": DataTypeCell.INT,
+        "calificador_de_frecuencia_respiratoria": DataTypeCell.STRING,
+        "temperatura_celsius": DataTypeCell.DECIMAL,
+        "peso_kg": DataTypeCell.DECIMAL,
+        "altura_metros": DataTypeCell.DECIMAL,
+        "saturacion_de_oxigeno": DataTypeCell.INT,
+        "perdida_de_conciencia": DataTypeCell.STRING,
+        "duracion_de_perdida_de_conciencia": DataTypeCell.TIME,
+        "gcs_motora": DataTypeCell.INT,
+        "gcs_ocular": DataTypeCell.INT,
+        "gcs_verbal": DataTypeCell.INT,
+        "gcs_total": DataTypeCell.INT,
+        "avup": DataTypeCell.STRING,
+      }
     },
 }
--- upload_manager/views.py
+++ upload_manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from datetime import datetime, date, time
 import tempfile
 import traceback
 import os
@@ -7,7 +7,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UploadSerializer
 from .helpers.gestor_excel_file_data import GestorExcelFileData, GestorExcelSheetData
-from .helpers.columns_configuration import columns_ubication
+from .helpers.columns_configuration import columns_ubication, column_name_type_to_model, DataTypeCell
+from django.db.models import Model
 
 class UploadView(APIView):
   
@@ -28,22 +29,16 @@ class UploadView(APIView):
     
     try:
       result = gestor_excel_file_data.get_data_from_file()
-      response_answer: dict[str,list[dict]] = {}
-      for key_sheet in result:
-        response_answer[key_sheet] = []
-        for row_data in result[key_sheet]:
-          patient = {**row_data}
-          response_answer[key_sheet].append(patient['trauma_register_record_id'])
-          # response_answer[key_sheet].append(patient)
-          # last_key = next(reversed(row_data.keys()))
-          
-          print(f"{key_sheet} {patient['trauma_register_record_id']}")
-        print("\n-----------------------\n")
-      
+      patients_key = 'Trauma Register:1'
+      patients_data: list[dict[str,str]] = result.pop(patients_key)
+      patients_result: dict[str, list[dict[str, str]]]  = {patients_key: patients_data}
+      self.save_elements_by_model(data_file=patients_result, column_name_type_to_model=column_name_type_to_model) 
+        
       if serializer.is_valid():
         return Response({
           "user_name": user_name,
-          "result": {**response_answer}
+          # "result": {**result},
+          "result": {**patients_result}
         }, status=status.HTTP_202_ACCEPTED)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
@@ -52,3 +47,67 @@ class UploadView(APIView):
     
     finally:
       os.remove(temp_file.name)
+  
+  def save_elements_by_model(self, data_file: dict[str, list[dict[str,str]]], column_name_type_to_model: dict[str,dict[str,Model|dict[str,DataTypeCell]]]):
+    try:
+      for key_file in data_file:
+        data_sheet = data_file[key_file]
+        model = column_name_type_to_model[key_file]["model"]
+        for data_row in data_sheet:
+          updated_data: dict[str,str|int|float|bool|datetime|date|time|None] = {}
+          for key_cell in data_row:
+            #! Here the data is converted according with their type
+            data_type_cell: DataTypeCell = column_name_type_to_model[key_file]["type"][key_cell]
+            data_cell: str | int | bool = data_row[key_cell].strip()
+            if data_cell == "None" or len(data_cell) == 0: continue
+            if data_type_cell == DataTypeCell.STRING or data_type_cell == DataTypeCell.TEXT:
+              updated_data[key_cell] = data_cell
+            elif data_type_cell == DataTypeCell.INT:
+              updated_data[key_cell] = int(data_cell)
+            elif data_type_cell == DataTypeCell.DECIMAL:
+              updated_data[key_cell] = float(data_cell)
+            elif data_type_cell == DataTypeCell.BOOLEAN:
+              boolean_result: bool | None
+              if (data_cell.lower() == "si" or data_cell.lower() == "sí"):
+                boolean_result = True
+              elif (data_cell.lower() == "no"):
+                boolean_result = False
+              else:
+                boolean_result = None
+              updated_data[key_cell] = boolean_result
+            elif data_type_cell == DataTypeCell.TIMESTAMP: #! It assumed that the format in excel file is saved as "DD/MM/YYYY hh:mm:ss" 
+              date_and_time = data_cell.split(" ")
+              obtained_date = date_and_time[0].split("/")
+              obtained_time = date_and_time[1].split(":")
+              obtained_date_time = datetime(
+                year=int(data[2]), 
+                month=int(data[1]), 
+                day=int(data[0]), 
+                hour=int(obtained_time[0]), 
+                minute=int(obtained_time[1]), 
+                second=int(obtained_time[2]),
+              )
+              updated_data[key_cell] = obtained_date_time
+            elif data_type_cell == DataTypeCell.TIME: #! It assumed that the format in excel file is saved as "hh:mm:ss"
+              obtained_time = data_cell.split(":")
+              obtained_date_time = time(
+                hour=int(obtained_time[0]), 
+                minute=int(obtained_time[1]), 
+                second=int(obtained_time[2]),
+              )
+              updated_data[key_cell] = obtained_date_time
+            elif data_type_cell == DataTypeCell.DATE: #! It assumed that the format in excel file is saved as "DD/MM/YYYY"
+              data = data_cell.split("/") 
+              if len(data) < 3: continue
+              obtained_date = date(
+                year=int(data[2]), 
+                month=int(data[1]), 
+                day=int(data[0]),
+              )
+              updated_data[key_cell] = obtained_date
+            else:
+              raise f"ERROR: DataTypeCell value doesn't exist, please establish the attribute {key_cell} in columns_configuration.py with DataTypeCell enum"
+          model(**updated_data).save()
+    except Exception as e:
+      print(F"ERROR(save_element_by_model): {type(e)} - {e}")
+      raise e
