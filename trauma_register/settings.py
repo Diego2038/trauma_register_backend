13146b15af737328e068cb2ef20d3265702ea74d
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2025-09-16 23:00:52 -0500

    2.6.1 Update: CORS allowed

--- trauma_register/settings.py
+++ trauma_register/settings.py
@@ -72,6 +72,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",  # Cambia esto por la URL de tu frontend en producción
     "http://127.0.0.1:8000",
     "http://localhost:3000",  # Si usas otro puerto en desarrollo
+    "https://diego2038.github.io",
+    "https://ephemeral-llama-5087ad.netlify.app",
     f"http://localhost:{variable_frontend_port}", # Para el puerto variable en Flutter
 ]
 
